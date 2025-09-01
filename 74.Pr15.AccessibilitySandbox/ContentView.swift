6f816ec9226a85101969a10bc3222e9221a57bc2
Valentin <pamerra@mail.ru>
2025-09-01 17:10:52 +0300

    Задал текст, который будет проговорен для изображений (вместо их названий (по умолчанию). Они будут прочтены как кнопки, и не прочтены как изображения

--- 74.Pr15.AccessibilitySandbox/ContentView.swift
+++ 74.Pr15.AccessibilitySandbox/ContentView.swift
@@ -15,6 +15,13 @@ struct ContentView: View {
         "nicolas-tissot-335096"
     ]
     
+    let labels = [
+        "Тюльпаны",
+        "Замерзшие почки деревьев",
+        "Подсолнухи",
+        "Фейерверк"
+    ]
+    
     @State private var selectedPicture = Int.random(in: 0...3)
     
     var body: some View {
@@ -24,6 +31,9 @@ struct ContentView: View {
             .onTapGesture {
                 selectedPicture = Int.random(in: 0...3)
             }
+            .accessibilityLabel(labels[selectedPicture])
+            .accessibilityAddTraits(.isButton)
+            .accessibilityRemoveTraits(.isImage)
     }
 }
 
