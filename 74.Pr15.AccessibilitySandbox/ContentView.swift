815aed5135a89ead40259f6d1d96b1eb0ab4c505
Valentin <pamerra@mail.ru>
2025-09-01 16:02:37 +0300

    Initial Commit

---
+++ 74.Pr15.AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  74.Pr15.AccessibilitySandbox
+//
+//  Created by Валентин on 01.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 74.Pr15.AccessibilitySandbox/_4_Pr15_AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_Pr15_AccessibilitySandboxApp.swift
+//  74.Pr15.AccessibilitySandbox
+//
+//  Created by Валентин on 01.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct _4_Pr15_AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
