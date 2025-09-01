cdcae25ad9e60e71c7050652ab933ccd1064bcaa
Valentin <pamerra@mail.ru>
2025-09-01 18:33:21 +0300

    74.2/7. ignore – дочерние элементы будут игнорироваться доступностью. Их всех можно заменить при помощощи .accessibilityLabel - такой текст будет читаться целиком

--- 74.Pr15.AccessibilitySandbox/ContentView.swift
+++ 74.Pr15.AccessibilitySandbox/ContentView.swift
@@ -16,7 +16,8 @@ struct ContentView: View {
             Text("1000")
                 .font(.title)
         }
-        .accessibilityElement(children: .combine)
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Your score is 1000")
     }
 }
 
